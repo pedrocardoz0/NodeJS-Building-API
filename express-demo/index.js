29c1c1f90a0648a6cd5189b34f9897a75fe86c10
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-06-30 15:24:33 -0300

    Add Simple Api Example

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,4 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get()
+app.get("/", (req, res) => {
+  res.send("Hello World !");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/post/:year/:month", (req, res) => {
+    res.send(req.params.year)
+})
+
+app.get("/api/postQ/:year/:month", (req, res) => {
+    res.send(req.query)
+})
+
+app.get
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server Listening ${PORT} ...`));
