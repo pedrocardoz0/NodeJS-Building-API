9c5b3cfe611238c76d649ff5612b9a0155f33428
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-06-30 15:32:13 -0300

    Add More Features

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send("The course ID was not found");
+  res.send(course);
 });
 
 app.get("/api/courses/:id", (req, res) => {
@@ -14,14 +27,14 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.get("/api/post/:year/:month", (req, res) => {
-    res.send(req.params.year)
-})
+  res.send(req.params.year);
+});
 
 app.get("/api/postQ/:year/:month", (req, res) => {
-    res.send(req.query)
-})
+  res.send(req.query);
+});
 
-app.get
+app.get;
 
 const PORT = process.env.PORT || 3000;
 
