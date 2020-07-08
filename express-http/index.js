49344224770e1c8e1264c841d7a31e3781eedfba
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-07-08 13:57:42 -0300

    Add Middleware Functionality

--- express-http/index.js
+++ express-http/index.js
@@ -1,8 +1,13 @@
 const Joi = require("joi")
+const logger = require("./logger")
+const auther = require("./auth")
 const express = require("express");
 const app = express();
 
 app.use(express.json())
+
+app.use(logger)
+app.use(auther)
 
 const courses = [
   { id: 1, name: 'c1' },
---
+++ express-http/logger.js
@@ -0,0 +1,8 @@
+const { func } = require("joi");
+
+function log(req, res, next) {
+    console.log("Logging ...")
+    next()
+}
+
+module.exports = log
