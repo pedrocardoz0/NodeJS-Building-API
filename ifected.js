e6b6e87f7e781b3a8bd1c5c1f3f7a7cc79abb6c7
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-07-15 13:56:01 -0300

    Add Some Files

---
+++ ifected.js
@@ -0,0 +1,23 @@
+[
+    {
+        'name': 'José Pereira',
+        'age': 20,
+        'contact': 3,
+        'id': 1,
+        'people_contact': ["Roberta Maria", "Ana Laura", "Jefferson Lins"]
+    },
+    {
+        'name': 'Jefferson Lins',
+        'age': 33,
+        'cotanct': 2,
+        'id': 2,
+        'people_contact': ["Ana Maria", "Laura Ishi"]
+    },
+    {
+        'name': 'Jefferson Lins',
+        'age': 46,
+        'cotanct': 1,
+        'id': 3,
+        'people_contact': ["Marco Assunção", "Nathan Lorenzo"]
+    }
+]
